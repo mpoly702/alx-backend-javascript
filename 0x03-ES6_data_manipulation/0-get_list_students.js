fa4c788b37979e81cf7434df371ac281aa413ada
Godfrey Oriazowan <mpoly702@gmail.com>
2024-08-14 05:49:25 +0100

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,8 @@
-const getListStudents = () => {
-let student1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
-let student2 = { id: 2, firstName: 'James', location: 'Columbia' };
-let student3 =  { id: 5, firstName: 'Serena', location: 'San Francisco'};
-  return [student1, student2, student3];
+export default const getListStudents = () => {
+const students = [
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' }, 
+  { id: 2, firstName: 'James', location: 'Columbia' }, 
+  { id: 5, firstName: 'Serena', location: 'San Francisco'};
+];
+  return students;
 };
-export default getListStudents;
